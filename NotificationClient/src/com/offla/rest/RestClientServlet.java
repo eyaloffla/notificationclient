1b3cee95d70f8160374948f0808668f6b83245d8
eyal <eyal.r.offla@gmail.com>
2015-12-03 18:02:58 +0200

    safas

--- NotificationClient/src/com/offla/rest/RestClientServlet.java
+++ NotificationClient/src/com/offla/rest/RestClientServlet.java
@@ -23,8 +23,8 @@ public class RestClientServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("===doGet===");
 		
-		String url= "https://mobile.ng.bluemix.net/imfpush/v1/apps/b70335bf-cd13-4fc2-86d3-03d53ec50487/messages";
-	    String message01= "{ \"message\": { \"alert\":\" Hi There whats up\"}}";
+		String url= "https://mobile.ng.bluemix.net/imfpush/v1/apps/49500464-58f0-4178-976d-a3d0b3c61170/messages";
+	    String message01= "{ \"message\": { \"alert\":\" http://offlafrontend.mybluemix.net/TextBitsMap\"}}";
 	    
 	     org.apache.wink.client.ClientConfig clientConfig = new org.apache.wink.client.ClientConfig();
 	     org.apache.wink.client.RestClient client= new org.apache.wink.client.RestClient(clientConfig);
@@ -32,7 +32,7 @@ public class RestClientServlet extends HttpServlet {
 	     resource.accept("application/json");
 	     resource.acceptLanguage("en-US");
 	     resource.contentType(MediaType.APPLICATION_JSON);
-	     resource.header("appSecret", "3021fa6c-5fa0-4149-ad4c-cee7160a8b34");
+	     resource.header("appSecret", "58533377-2567-47db-9b5b-69237b8eb199");
 	     resource.header("Application-Mode", "SANDBOX");
 	    
 	     org.apache.wink.client.ClientResponse resp= resource.post(message01);
@@ -48,23 +48,5 @@ public class RestClientServlet extends HttpServlet {
 	}
 	    
 	    
-	    /*
-	    
-	   org.apache.wink.client.ClientConfig clientConfig = new org.apache.wink.client.ClientConfig();
-		
-		org.apache.wink.client.RestClient client = new org.apache.wink.client.RestClient(clientConfig);
-		org.apache.wink.client.Resource resource = client.resource("http://restservicessamples.mybluemix.net/banking/Greeting");
-		org.apache.wink.client.ClientResponse resp = resource.get();
-		String msg = (String) resp.getMessage();
-		System.out.println(msg);
-		System.out.println(resp.getStatusCode());
-		System.out.println(resp.getEntity(String.class));
-		
-		java.io.PrintWriter printWriter = response.getWriter();		
-		printWriter.println(resp.getEntity(String.class));
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	*/
 
 }
